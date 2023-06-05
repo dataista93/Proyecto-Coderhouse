cc9f05c31a89008f238ab50cb71727f35d99d936
dataista93 <135561984+dataista93@users.noreply.github.com>
2023-06-05 11:07:34 -0300

    Add files via upload

--- data_storytelling+alexander_sothmann.py
+++ data_storytelling+alexander_sothmann.py
@@ -25,7 +25,7 @@ def load_data(nrows):
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+data = load_data(1000000)
 data_load_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data'):
