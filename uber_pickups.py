5b7585c6cb6f5ea76ddb2d4392e2069163c341c7
dataista93 <135561984+dataista93@users.noreply.github.com>
2023-06-04 23:02:14 -0300

    Add files via upload

--- uber_pickups.py
+++ uber_pickups.py
@@ -15,6 +15,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+'''
+## This is the document title
+#####dfdsgfg
+
+This is some _markdown_.
+'''
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
